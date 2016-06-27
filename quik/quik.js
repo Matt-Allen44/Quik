e1b4ecd9c9a19797c30eed1995644e92dc826f34
Matthew Allen <whosdaz@gmail.com>
2016-06-27 20:49:53 +0930

    Reformated and added quik.css

--- quik/quik.js
+++ quik/quik.js
@@ -3,12 +3,16 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var sanitizeHtml = require('sanitize-html');
 
+app.get('/css/quik.css', function(req, res){
+	res.sendFile(__dirname + '/css/quik.css'); //CHANGE TO QUIK.HTML LATER
+});
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/chat.html'); //CHANGE TO QUIK.HTML LATER
 });
 
-http.listen(25565, function(){
-	console.log('Launched Quik on :25565')
+http.listen(80, function(){
+	console.log('Launched Quik on :80')
 });
 
 usrs_connected = 0;
@@ -17,24 +21,24 @@ usrs_connected = 0;
 /*
 	ON CONNECTION
 */
-io.on('connection', function(socket){
-	usrs_connected = usrs_connected+1;
-	io.emit('connectEvent', usrs_connected)
+	io.on('connection', function(socket){
+		usrs_connected = usrs_connected+1;
+		io.emit('connectEvent', usrs_connected)
 
 
-	socket.on('disconnect', function(){
-		usrs_connected = usrs_connected-1;
-		console.log(usrs_connected + ' users connected')
-		io.emit('disconnectEvent', usrs_connected)
-	})
+		socket.on('disconnect', function(){
+			usrs_connected = usrs_connected-1;
+			console.log(usrs_connected + ' users connected')
+			io.emit('disconnectEvent', usrs_connected)
+		})
 
-	socket.on('chat message', function(msg){
-		msg = sanitizeHtml(msg)
-		if(msg == ""){
-			console.log("> empty message removed")
-		} else {
-			console.log('> ' + msg)
-			io.emit('chat message', msg)
-		}
-	})
+		socket.on('chat message', function(msg){
+			msg = sanitizeHtml(msg)
+			if(msg == ""){
+				console.log("> empty message removed")
+			} else {
+				console.log('> ' + msg.replace("--DELIM--", ":"))
+				io.emit('chat message', msg)
+			}
+		})
 });
