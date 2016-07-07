295f92e754f0eed87901fb6fadeb8d335281f252
Matthew Allen <whosdaz@gmail.com>
2016-07-07 16:04:49 +0930

    Changed IP Lookup, modified client logging

--- quik.js
+++ quik.js
@@ -5,6 +5,9 @@ var sanitizeHtml = require('sanitize-html');
 var geoip = require('geoip-lite');
 var swearjar = require('swearjar');
 
+var clients = [];
+var usrs_connected = 0;
+
 app.get('/css/quik.css', function(req, res){
 	res.sendFile(__dirname + '/css/quik.css');
 });
@@ -13,12 +16,18 @@ app.get('/branding/favicon.ico', function(req, res){
 	res.sendFile(__dirname + '/branding/favicon.ico');
 });
 
+
+/* Dashboard info */
 app.get('/dash/sysdat', function(req, res){
 	if(process.platform === 'win32'){
 		res.send("501 Not Implemented (SYSDAT NOT SUPPORTED ON WINDOWS)")
 	} else {
 		res.send("200 OK");
 	}
+});
+
+app.get('/dash/usrdat', function(req, res){
+		res.send(clients.toString())
 });
 
 process.platform === 'win32'
@@ -42,7 +51,6 @@ app.get('/firebase', function(req, res){
 	res.sendFile(__dirname + '/firebase.html');
 });
 
-var clients = [];
 app.get('/dash', function(req, res){
 	res.sendFile(__dirname + '/dash.html');
 });
@@ -59,9 +67,6 @@ http.listen(80, function(){
 	console.log('Launched Quik on :80')
 });
 
-usrs_connected = 0;
-
-
 /*
 	ON CONNECTION
 */
@@ -70,7 +75,7 @@ usrs_connected = 0;
 		socket.emit('chat message', "Notice --DELIM-- This application is released under the Apache 2.0 License, hack on the source at https://github.com/Matt-Allen44/Quik")
 		socket.emit('chat message', 'Notice --DELIM-- This chat room is logged and users must comply with the TOS')
 
-		clients.push(socket)
+		clients.push([socket.conn.remoteAddress, ipLookup(socket.conn.remoteAddress)["city"]])
 		usrs_connected = usrs_connected+1;
 		io.emit('connectEvent', usrs_connected)
 
@@ -102,8 +107,20 @@ usrs_connected = 0;
   				socket.emit('chat message', "Server --DELIM-- Empty message removed")
   				console.log("> empty message removed")
   			} else {
-  				console.log(socket.conn.remoteAddress + " [" + /*geoip.lookup(socket.conn.remoteAddress)["city"] +*/ "] " + msg.replace("--DELIM--", ":"))
+  				console.log(socket.conn.remoteAddress + " [" + ipLookup(socket.conn.remoteAddress)["city"] + "] " + msg.replace("--DELIM--", ":"))
   				io.emit('chat message', swearjar.censor(msg))
   			}
 		})
 });
+
+
+/
+function ipLookup(ip){
+	if(ip == "127.0.0.1" || ip == "localhost" || ip == "::1"){
+		var array = {city: "Local Client"}
+		return array
+	} else {
+		console.log(ip)
+		return geoip.lookup(ip)
+	}
+}
