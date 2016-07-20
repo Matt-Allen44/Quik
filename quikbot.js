bb52588554ce134b45e71fdd99721d4bff66074d
Matthew Allen <whosdaz@gmail.com>
2016-07-21 00:48:44 +0930

    Fixed linter issues

--- quikbot.js
+++ quikbot.js
@@ -1,15 +1,14 @@
 var io;
 module.exports = function(importIO) {
-    console.log("Quikbot.js loaded")
+    console.log("Quikbot.js loaded");
     io = importIO;
-}
+};
 
 
 module.exports.broadcast = function(text) {
-  console.log("Emitting")
   io.emit('chat message', "Quikbot", text);
 };
 
 module.exports.test = function(){
   console.log("test");
-}
+};
